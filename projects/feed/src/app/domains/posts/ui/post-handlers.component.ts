1be73193b3c54075d876283da0f3cc00546886d7
OUSSAMA YAAGOUB <oyaagoub@externas.sescam.jccm.es>
2025-09-14 13:15:50 +0200

    update .firebaserc, firebase-deploy-hosting.yml, firebase.json and post-handlers.component.ts

--- projects/feed/src/app/domains/posts/ui/post-handlers.component.ts
+++ projects/feed/src/app/domains/posts/ui/post-handlers.component.ts
@@ -7,24 +7,24 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
   <div class="flex justify-between py-5 text-gray-500 md:space-x-4 md:justify-start">
-  <button class="flex items-center px-2 py-3 space-x-2 font-medium transition duration-200 rounded-md hover:bg-gray-200">
-    <i class="fa-solid fa-thumbs-up text-lg"></i>
-    <p>Like</p>
-  </button>
-  <button class="flex items-center px-2 py-3 space-x-2 font-medium transition duration-200 rounded-md hover:bg-gray-200">
-    <i class="fa-solid fa-comment text-lg"></i>
-    <p>Comment</p>
-  </button>
-  <button class="flex items-center px-2 py-3 space-x-2 font-medium transition duration-200 rounded-md hover:bg-gray-200">
-    <i class="fa-solid fa-share text-lg"></i>
-    <p>Share</p>
-  </button>
-  <div class="hidden md:block">
     <button class="flex items-center px-2 py-3 space-x-2 font-medium transition duration-200 rounded-md hover:bg-gray-200">
-      <i class="fa-solid fa-paper-plane text-lg"></i>
-      <p>Send</p>
+      <i class="fa-solid fa-thumbs-up text-lg"></i>
+      <p>Like</p>
     </button>
-  </div>
+    <button class="flex items-center px-2 py-3 space-x-2 font-medium transition duration-200 rounded-md hover:bg-gray-200">
+      <i class="fa-solid fa-comment text-lg"></i>
+      <p>Comment</p>
+    </button>
+    <button class="flex items-center px-2 py-3 space-x-2 font-medium transition duration-200 rounded-md hover:bg-gray-200">
+      <i class="fa-solid fa-share text-lg"></i>
+      <p>Share</p>
+    </button>
+    <div class="hidden md:block">
+      <button class="flex items-center px-2 py-3 space-x-2 font-medium transition duration-200 rounded-md hover:bg-gray-200">
+        <i class="fa-solid fa-paper-plane text-lg"></i>
+        <p>Send</p>
+      </button>
+    </div>
 </div>
 
   `
